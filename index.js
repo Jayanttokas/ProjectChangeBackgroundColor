966a4ae3474baa2e195fc8ab6369def73cabcd68
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-04-18 21:42:01 +0530

    files for background changing

---
+++ index.js
@@ -0,0 +1,37 @@
+var changeColor = (function(){
+
+    const eventHandler = () => {
+        // Everytime time happens Event is called
+        document.getElementById("change").addEventListener("click", changeBackgroundColor);
+    };
+
+    const changeBackgroundColor = () => {
+      // Calls function to generate new color
+      let colors = colorGenerator();
+
+      // Update the new color to background
+      document.getElementById("background").style.background=colors;
+    };
+
+    // Generates a random color everytime button is clickd
+    const colorGenerator = () => {
+          let letters = '0123456789ABCDEF';
+          let color = '#';
+          for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+        };
+
+    return{
+      // Initialization Function
+      init: function() {
+        console.log("app started");
+        // Calls the event Handler
+        eventHandler()
+      }
+    };
+
+})();
+
+changeColor.init();
